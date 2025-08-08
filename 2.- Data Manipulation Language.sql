89d0a34dc22430bfea56201c2aa16f3e2a530653
DaroGard <alexdg997@gmail.com>
2025-08-07 21:26:34 -0600

    v1.0 Release

--- 2.- Data Manipulation Language.sql
+++ 2.- Data Manipulation Language.sql
@@ -3,3 +3,6 @@ VALUES ('POKE QUEUE REQUEST');
 
 INSERT INTO pokequeue.[status] ([description])
 VALUES ('sent'), ('inprogress'), ('completed'), ('failed');
+
+ALTER TABLE pokequeue.requests
+ADD sample_size INT NULL; -- #tarea 3: Almacena este valor en una nueva columna en la tabla de reportes en Azure SQL DB (ej. SampleSize INT NULL ).
--- 3.- Stored Procedures.sql
+++ 3.- Stored Procedures.sql
@@ -16,3 +16,42 @@ INSERT INTO pokequeue.requests(
 )
 
 SELECT max(id) as id FROM pokequeue.requests;
+
+CREATE PROCEDURE pokequeue.update_poke_request(
+    @id int
+    , @status NVARCHAR(255)
+    , @url NVARCHAR(1000)
+)
+AS
+
+SET NOCOUNT ON;
+
+UPDATE pokequeue.requests
+SET id_status = (SELECT id FROM  pokequeue.status where description = @status)
+    ,url = @url
+    ,updated = GETDATE()
+WHERE id = @id
+
+SELECT 1 as completed;
+
+ALTER PROCEDURE pokequeue.create_poke_request(
+    @type NVARCHAR(255),
+    @sample_size INT = NULL  -- #tarea 3: Almacena este valor en una nueva columna en la tabla de reportes en Azure SQL DB (ej. SampleSize INT NULL ).
+)
+AS
+
+SET NOCOUNT ON;
+
+INSERT INTO pokequeue.requests(
+    [type],
+    [url],
+    id_status,
+    sample_size    -- #tarea 3: Almacena este valor en una nueva columna en la tabla de reportes en Azure SQL DB (ej. SampleSize INT NULL ).
+) VALUES(
+    @type,
+    '',
+    (SELECT id FROM pokequeue.status WHERE description = 'sent'),
+    @sample_size
+)
+
+SELECT MAX(id) AS id FROM pokequeue.requests;
